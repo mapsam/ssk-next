c6874691012d0ed8d1a165a3e2a0d61c0023123a
mapsam <mapsam@users.noreply.github.com>
2024-10-11 20:02:32 +0000

    deploy: 1711943859ed7fd384769d30694ddf95addb87b1

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,24 @@
-import sharp from 'sharp';
-import Image from '@11ty/eleventy-img';
 import { EleventyHtmlBasePlugin } from '@11ty/eleventy';
+import eleventyNavigationPlugin from '@11ty/eleventy-navigation';
+import { featuredImageShortcode } from './lib/shortcode-featured-image.js';
+import { imageShortcode } from './lib/shortcode-image.js';
+import { specificationShortcode } from './lib/shortcode-specification.js';
+import { PATH_PREFIX } from './lib/constants.js';
 import esbuild from 'esbuild';
-import { writeFile, mkdir } from 'node:fs/promises';
-import path from 'node:path';
 
 export const config = {
-  pathPrefix: '/ssk-next',
+  pathPrefix: PATH_PREFIX,
 };
 
 export default function (conf) {
-  // copy css
   conf.addPassthroughCopy('assets/css');
-
-  // bundle javascript
   conf.addTemplateFormats('js');
+  conf.addPlugin(eleventyNavigationPlugin);
+  conf.addPlugin(EleventyHtmlBasePlugin);
+  conf.addCollection('projects', (coll) => coll.getFilteredByGlob('projects/**/*.md'));
+  conf.addShortcode('featuredimage', featuredImageShortcode);
+  conf.addShortcode('image', imageShortcode);
+  conf.addShortcode('specification', specificationShortcode);
   conf.on('eleventy.before', async () => {
     await esbuild.build({
       entryPoints: ['assets/js/ssk.js'],
@@ -24,87 +28,4 @@ export default function (conf) {
       target: ['chrome58', 'firefox57', 'safari11', 'edge16'],
     });
   });
-
-  // featured image shortcode
-  conf.addShortcode('featuredimage', async function (src) {
-    const width = 400;
-    const height = 256;
-
-    const image = sharp(src).resize({
-      fit: sharp.fit.cover,
-      width,
-      height,
-    });
-
-    const featuredImage = await image.toFormat('jpeg').toBuffer();
-    const placeholder = await image.blur(35).toBuffer();
-    const base64Placeholder = `data:image/png;base64, ${placeholder.toString('base64')}`;
-    const [filename, suffix] = path.basename(src).split('.');
-    const outputUrl = `./_site/assets/img/${filename}_featured.${suffix}`;
-    const url = path.join(
-      config.pathPrefix,
-      `/assets/img/${filename}_featured.${suffix}`,
-    );
-
-    await mkdir('./_site/assets/img/', { recursive: true });
-    await writeFile(outputUrl, featuredImage);
-
-    const img = `<img
-			class="lazy"
-			alt="featured-image"
-			src="${base64Placeholder}"
-			data-src="${url}"
-			width="${width}"
-			height="${height}"
-		>`;
-
-    return `<div class="featured-image">${img}</div>`;
-  });
-
-  // optimize image sizes and create lazy-load ready html elements
-  // h/t https://maw.sh/blog/how-to-optimize-and-lazyloading-images-on-eleventy/
-  conf.addShortcode('image', async function (src, alt, featured = false) {
-    const stats = await Image(src, {
-      widths: [512, 1024],
-      formats: ['jpeg', 'webp'],
-      urlPath: '/assets/img/',
-      outputDir: './_site/assets/img/',
-    });
-
-    const lowestSrc = stats['jpeg'][0];
-
-    const sourceSet = {};
-    for (let fmt in stats) {
-      sourceSet[fmt] = stats[fmt]
-        .map(({ srcset }) => path.resolve(`${config.pathPrefix}${srcset}`))
-        .join(',');
-    }
-
-    const placeholder = await sharp(lowestSrc.outputPath)
-      .resize({ fit: sharp.fit.inside })
-      .blur(35)
-      .toBuffer();
-
-    const base64Placeholder = `data:image/png;base64, ${placeholder.toString('base64')}`;
-    const source = `<source 
-			type="image/webp"
-			data-srcset="${sourceSet['webp']}"
-			data-sizes="(min-width: 1024px) 1024px, 100vw"
-		>`;
-
-    const img = `<img
-			class="lazy"
-			alt="${alt}"
-			src="${base64Placeholder}"
-			data-src="${lowestSrc.url}"
-			data-sizes="(min-width: 1024px) 1024px, 100vw"
-			data-srcset="${sourceSet['jpeg']}"
-			width="${lowestSrc.width}"
-			height="${lowestSrc.height}"
-		>`;
-
-    return `<div class="image-wrapper"><picture>${source}${img}</picture></div>`;
-  });
-
-  conf.addPlugin(EleventyHtmlBasePlugin);
 }
